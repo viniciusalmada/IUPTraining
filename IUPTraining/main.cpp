416bab0256fc27d7cb61dee8ae58eab3012d6d53
Vinicius Almada <viniciusalmada10@gmail.com>
2021-09-06 22:02:36 -0300

    add buton and callback

--- IUPTraining/main.cpp
+++ IUPTraining/main.cpp
@@ -1,10 +1,34 @@
 #include <cstdlib>
 #include "iup.h"
 
+int ButtonExitCb(Ihandle* self)
+{
+  if (self)
+    return IUP_CLOSE;
+  return IUP_ERROR;
+}
+
 int main()
 {
   IupOpen(nullptr, nullptr);
-  IupMessage("Windows title", "Hello, World from IUP");
+
+  Ihandle* label = IupLabel("Hello world from IUP");
+  Ihandle* button = IupButton("OK", nullptr);
+  Ihandle* vbox = IupVbox(label, button, nullptr);
+
+  IupSetAttribute(vbox, "ALIGNMENT", "ACENTER");
+  IupSetAttribute(vbox, "GAP", "10");
+  IupSetAttribute(vbox, "MARGIN", "10x10");
+
+  Ihandle* dlg = IupDialog(vbox);
+  IupSetAttribute(dlg, "TITLE", "Hello world");
+
+  IupSetCallback(button, "ACTION", ButtonExitCb);
+
+  IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
+
+  IupMainLoop();
+
   IupClose();
   return EXIT_SUCCESS;
 }
